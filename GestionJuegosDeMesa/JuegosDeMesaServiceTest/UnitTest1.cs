dd84eee8f8bc913f76d767a47e5801380d236001
tavani531 <santitavani7@gmail.com>
2025-05-28 17:14:45 -0300

    Tercer Commit

---
+++ GestionJuegosDeMesa/JuegosDeMesaServiceTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace JuegosDeMesaServiceTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
